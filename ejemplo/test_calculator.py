286a6c47166e53359c52000631024f970f39c674
rodricar99 <39935861+rodricar99@users.noreply.github.com>
2023-07-08 02:34:43 -0500

    Update test_calculator.py

--- ejemplo/test_calculator.py
+++ ejemplo/test_calculator.py
@@ -1,3 +1,9 @@
+# test_calculator.py
+import coverage
+
+cov = coverage.Coverage()
+cov.start()
+
 def add_numbers(a, b):
     return a + b
 
@@ -5,3 +11,8 @@ def test_add_numbers():
     assert add_numbers(2, 3) == 5
     assert add_numbers(-1, 1) == 0
     assert add_numbers(0, 0) == 0
+
+if __name__ == "__main__":
+    cov.stop()
+    cov.save()
+    cov.report()
